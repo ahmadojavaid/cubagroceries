a46b6d1d50d5327c175534bc7f5e233e942bc295
Ahmad Javaid <ahmadomer63@gmail.com>
2026-03-05 01:43:45 +0500

    feat: GlitchTip/Sentry integration for backend and mobile
    - backend: composer require sentry/sentry-laravel
    - mobile: flutter pub get (sentry_flutter, sentry_dio)
    - native splash regenerated with white background

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,8 @@ import firebase_core
 import firebase_messaging
 import flutter_secure_storage_darwin
 import geolocator_apple
+import package_info_plus
+import sentry_flutter
 import sqflite_darwin
 import url_launcher_macos
 
@@ -19,6 +21,8 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
