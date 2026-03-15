b06b763d72b07fdfa102fb7f7af0612eb30868b9
Ahmad Javaid <ahmadomer63@gmail.com>
2026-03-15 07:55:02 +0500

    fix(api): serialize date_of_birth as Y-m-d to prevent timezone shift

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
     {
         return [
             'password' => 'hashed',
-            'date_of_birth' => 'date',
+            'date_of_birth' => 'date:Y-m-d',
             'wallet_amount' => 'decimal:2',
         ];
     }
